161d1972744c21addc12793abfd34b1ed133c795
Johann Hospice <johannhospice.dev@gmail.com>
2017-04-13 03:39:12 +0200

    Move uikit js files in a directory

--- public/js/client.js
+++ public/js/client.js
@@ -1,117 +1,196 @@
+Math.round5 = function (x) {
+    return this.ceil(x / 5) * 5;
+}
+Number.prototype.toStringFormated = function (length) {
+    var value = this.valueOf()
+    if (value <= 0) return '00'
+    var str = ""
+    for (let i = length - 1; value < Math.pow(10, i); i--)
+        str += '0'
+    str += value
+    return str
+}
+
+/**
+ * 
+ */
 $(document).ready(function () {
-    const tools = {
-        ajaxFactory: {
-            route: function (departure, arrival) {
-                return $.ajax('/query/route', {
-                    type: 'GET',
-                    data: {
-                        arrival,
-                        departure
-                    },
-                    // use done failure instead
-                    success: function (data) {
-                        $('input[name=departure]')
-                            .removeClass('uk-form-danger')
-                            .addClass('uk-form-success')
-                        $('input[name=arrival]')
-                            .removeClass('uk-form-danger')
-                            .addClass('uk-form-success')
-                        $('#result').html(data)
-                    },
-                    error: function(info, info2){
-                        UIkit.notify({
-                            message: searchErrorString(
-                                info.responseJSON.departure, 
-                                info.responseJSON.arrival),
-                            status: 'danger',
-                            timeout: 5000,
-                            pos: 'top-center'
-                        });
-                        console.log(info.responseJSON)
-                        if(info.responseJSON.departure)
-                            $('input[name=departure]')
-                                .addClass('uk-form-danger')
-                        if(info.responseJSON.arrival)
-                            $('input[name=arrival]')
-                                .addClass('uk-form-danger')
-                    },
-                })
-            },
-            search: function (location, success, error) {
-                return $.ajax('/query/search', {
-                    type: 'GET',
-                    data: {location},
-                    success,
-                    error,
-                })
+    /**
+     * 
+     */
+    function RequestFactory() {}
+    RequestFactory.prototype.route = function (departure, arrival, sens, time) {
+        return $.ajax({
+            url: '/query/route',
+            type: 'GET',
+            data: {
+                arrival,
+                departure,
+                sens,
+                time
             }
-        },
-        validateInputs: function ($inputs) {
-            var valid = true
-            $inputs.each(function () {
-                var $this = $(this)
-                var value = $this.val()
-                if (typeof value !== 'string' || value.length == 0) {
-                    $this.removeClass('uk-form-success')
-                    $this.addClass('uk-form-danger')
-                    valid = false
-                }
-            });
-            return valid
-        }
+        })
+    }
+    RequestFactory.prototype.search = function (location) {
+        return $.ajax({
+            url: '/query/search',
+            type: 'GET',
+            data: {
+                location
+            }
+        })
+    }
+    var requestFactory = new RequestFactory()
+
+    /**
+     * Static Func
+     */
+    function setClassDanger($input) {
+        return $input.removeClass('uk-form-success').addClass('uk-form-danger')
     }
 
-    $('input').keyup(function () {
+    function setClassSuccess($input) {
+        return $input.removeClass('uk-form-danger').addClass('uk-form-success')
+    }
+
+    function inputKeyup() {
         $(this).removeClass('uk-form-danger')
+    }
+
+    /**
+     * Init vars
+     */
+    const $route = $('#route')
+    const $result = $route.find('#result')
+    const $form = $route.find('#form')
+
+    const $timeContainer = $form.find('#time-container')
+    const $sens = $form.find('[name=sens]')
+    const $inputs = $form.find('input[type=text]')
+
+    const $departure = $inputs.filter('[name=departure]')
+    const $arrival = $inputs.filter('[name=arrival]')
+    const $time = $inputs.filter('[name=time]')
+
+    /**
+     * Init elements
+     */
+    ;
+    (function () {
+        var now = new Date()
+        var minutes = Math.round5(now.getMinutes()).toStringFormated(2)
+        var hours = now.getHours().toStringFormated(2)
+        $time.val(hours + ':' + minutes)
+    })()
+
+    /**
+     * Init behaviors
+     */
+    $inputs.keyup(inputKeyup)
+
+    $sens.change(function () {
+        var value = $(this).val()
+        if (value === "now") {
+            $timeContainer.addClass('uk-hidden')
+            $time.removeClass('uk-form-danger')
+        } else if (value === "leave" || value === "arrive") {
+            $timeContainer.removeClass('uk-hidden')
+        }
     })
-    $('#route form').submit(function (event) {
-        event.preventDefault();
-        var $inputs = $(this).find('input[type=text]')
-        if (tools.validateInputs($inputs))
-            tools.ajaxFactory.route(
-                $inputs.filter('[name=departure]').val(), 
-                $inputs.filter('[name=arrival]').val())
-        else
+
+    /**
+     * Request Submited to get Route
+     */
+    $form.submit(function (event) {
+        event.preventDefault()
+
+        var departure = $departure.val()
+        var arrival = $arrival.val()
+        var sens = $sens.val()
+
+        var notNow = sens == 'arrive' || sens == 'leave'
+        var time = null
+        var valid = true
+
+        if (typeof departure !== 'string' || departure.length == 0) {
+            setClassDanger($departure)
+            valid = false
+        }
+
+        if (typeof arrival !== 'string' || arrival.length == 0) {
+            setClassDanger($arrival)
+            valid = false
+        }
+
+        if (notNow) {
+            try {
+                var timeValue = $time.val()
+                var timeValueSplited = timeValue.split(':')
+                var hours = parseInt(timeValueSplited[0], 10)
+                var minutes = parseInt(timeValueSplited[1], 10)
+                if (timeValue.length == 5 && minutes != NaN && hours != NaN) {
+                    time = [hours, minutes]
+                } else throw 'err'
+            } catch (e) {
+                setClassDanger($time)
+                valid = false
+            }
+        } else {
+            var date = new Date()
+            time = [date.getHours(), date.getMinutes()]
+        }
+
+        if (valid) {
+            requestFactory.route(departure, arrival, sens, time)
+                .done(function (data) {
+                    setClassSuccess($departure)
+                    setClassSuccess($arrival)
+                    if (notNow)
+                        setClassSuccess($time)
+                    $result.html(data)
+                })
+                .fail(function (jqXHR) {
+                    var departure = jqXHR.responseJSON.departure,
+                        arrival = jqXHR.responseJSON.arrival
+                    if (!departure) setClassDanger($departure)
+                    if (!arrival) setClassDanger($arrival)
+                    UIkit.notify({
+                        message: "Station(s) do(es) not exist(s)",
+                        status: 'danger',
+                        timeout: 5000,
+                        pos: 'top-center'
+                    });
+                })
+        } else {
             UIkit.notify({
                 message: "Please fill in the fields",
                 status: 'danger',
-                timeout: 5000,
-                pos: 'top-center'
+                pos: 'top-center',
+                timeout: 5000
             });
+        }
     })
 
-    $('#search form').submit(function (event) {
-        event.preventDefault();
-        var $inputs = $(this).find('input[type=text]')
-        if (tools.validateInputs($inputs))
-            tools.ajaxFactory.search($inputs.find('[name=location]').val())
-    })
-    var autocompleteOptions = {
+    /**
+     * Autocompletion
+     */
+    const autocompleteOptions = {
+        'minLength': 1,
         'source': function (release) {
-            tools.ajaxFactory.search(
-                this.input.val(),
-                function(data){
-                    release(data.map(function(obj){
-                        return{value: obj.label}
+            let value = this.input.val()
+            requestFactory.search(value)
+                .done(function (stations) {
+                    release(stations.map(function (station) {
+                        return {
+                            value: station.label
+                        }
                     }))
-                },
-                function(){release([])})
-        },
-        'minLength': 1
+                })
+                .fail(function () {
+                    release([])
+                })
+        }
     }
     $.UIkit.autocomplete($('#autocomplete-departure'), autocompleteOptions);
     $.UIkit.autocomplete($('#autocomplete-arrival'), autocompleteOptions);
-
 })
-
-function searchErrorString(departure, arrival){
-    var str = "These field locations do not exist: "
-    if(!arrival){
-        str += 'departure'
-        if(!departure)
-            str +=', arrival'
-    }
-    else if(!departure)
-        str +='arrival'
-    return str
-}
