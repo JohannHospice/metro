a94a1385498e195c1fb94f85b31ecd4addc28024
Johann Hospice <johannhospice.dev@gmail.com>
2017-04-16 04:11:28 +0200

    Various modification - update metro assets - refactor client

--- public/js/client.js
+++ public/js/client.js
@@ -3,9 +3,9 @@ Math.round5 = function (x) {
 }
 Number.prototype.toStringFormated = function (length) {
     var value = this.valueOf()
-    if(value < 0)
+    if (value < 0)
         throw 'not my problem'
-    if (value == 0) 
+    if (value == 0)
         return '00'
     var str = ""
     for (let i = length - 1; value < Math.pow(10, i); i--)
@@ -14,40 +14,110 @@ Number.prototype.toStringFormated = function (length) {
     return str
 }
 
+
+
 $(document).ready(function () {
     // Static Func
-    function setClassDanger($input) {
-        return $input.removeClass('uk-form-success').addClass('uk-form-danger')
+    function setClassDanger($element) {
+        return $element.removeClass('uk-form-success').addClass('uk-form-danger')
     }
 
-    function setClassSuccess($input) {
-        return $input.removeClass('uk-form-danger').addClass('uk-form-success')
+    function setClassSuccess($element) {
+        return $element.removeClass('uk-form-danger').addClass('uk-form-success')
+    }
+
+    function HTMLBuilder() {
+        var html = ""
+        return {
+            add: function (elem, str) {
+                this.open(elem).text(str).close(elem)
+                return this
+            },
+            open: function (elem) {
+                html += '<' + elem + '>'
+                return this
+            },
+            close: function (elem) {
+                html += '</' + elem + '>'
+                return this
+            },
+            text: function (str) {
+                html += str
+                return this
+            },
+            build: function () {
+                return html
+            }
+        }
+    }
+
+    function formValidator(departure, arrival, time, sens) {
+        var notNow = sens == 'arrive' || sens == 'leave'
+        var timeParsed = notNow ? Time.parse(time) : Time.parseByDate(new Date())
+        var valid = {
+            arrival: arrival.length != 0,
+            departure: departure.length != 0,
+            diffRouteArrival: arrival !== departure,
+            time: Time.isValid(timeParsed)
+        }
+        valid.all = valid.time && valid.diffRouteArrival && valid.departure && valid.arrival
+        return {
+            data: {
+                time: timeParsed,
+                departure,
+                notNow,
+                arrival,
+                sens
+            },
+            valid
+        }
     }
 
     // RequestFactory
-    function RequestFactory() {}
-    RequestFactory.prototype.route = function (departure, arrival, sens, time) {
-        return $.ajax({
-            url: '/query/route',
-            type: 'GET',
-            data: {
+    const requestFactory = {
+        ajax: function (url, type, data) {
+            return $.ajax({
+                url,
+                type,
+                data
+            })
+        },
+        route: function (departure, arrival, sens, time) {
+            return this.ajax('/query/route', 'GET', {
                 arrival,
                 departure,
                 sens,
                 time
-            }
-        })
-    }
-    RequestFactory.prototype.search = function (location) {
-        return $.ajax({
-            url: '/query/search',
-            type: 'GET',
-            data: {
+            })
+        },
+        search: function (location) {
+            return this.ajax('/query/search', 'GET', {
                 location
-            }
-        })
+            })
+        }
     }
-    var requestFactory = new RequestFactory()
+
+    const Time = {
+        composeByDate: function (date) {
+            return date.getHours() + ':' + date.getMinutes()
+        },
+        compose: function (hours, minutes) {
+            return hours.toStringFormated(2) + ':' + minutes.toStringFormated(2)
+        },
+        parseByDate: function (date) {
+            return [date.getHours(), date.getMinutes()]
+        },
+        parse: function (timeStr) {
+            if (timeStr.length != 5)
+                return [NaN, NaN]
+            return timeStr.split(':').map(function (time) {
+                return parseInt(time)
+            })
+        },
+        isValid: function (time) {
+            return !Number.isNaN(time[0]) && time[0] < 24 && !Number.isNaN(time[1]) && time[1] < 60
+        }
+    }
 
     // Init vars
     const $route = $('#route')
@@ -67,17 +137,16 @@ $(document).ready(function () {
     ;
     (function () {
         var now = new Date()
-        var minutes = Math.round5(now.getMinutes()).toStringFormated(2)
-        var hours = now.getHours().toStringFormated(2)
-        $time.val(hours + ':' + minutes)
+        now.setMinutes(Math.round5(now.getMinutes()))
+        $time.val(Time.composeByDate(now))
     })()
 
     // Autocompletion
     ;
     (function () {
         const options = {
-            'minLength': 1,
-            'source': function (release) {
+            minLength: 1,
+            source: function (release) {
                 let value = this.input.val()
                 requestFactory.search(value)
                     .done(function (stations) {
@@ -97,11 +166,28 @@ $(document).ready(function () {
         })
     })()
 
+    function updateHandler() {
+        var $this = $(this)
+        if ($this.hasClass('uk-form-danger'))
+            $this.removeClass('uk-form-danger')
+    }
     // Init event handlers
-    $inputs.keyup(function () {
-        $(this).removeClass('uk-form-danger')
+    // for user
+    $inputs.keyup(updateHandler)
+    $time.keyup(function () {
+        var $this = $(this)
+        if (!Time.isValid(Time.parse($this.val())))
+            $this.addClass('uk-form-danger')
+        else if ($this.hasClass('uk-form-danger'))
+            $this.removeClass('uk-form-danger')
     })
-
+    //for timepicker
+    $time.change(function () {
+        var $this = $(this)
+        if ($this.hasClass('uk-form-danger'))
+            $this.removeClass('uk-form-danger')
+    })
+    
     $sens.change(function () {
         var value = $(this).val()
         if (value === "now") {
@@ -115,74 +201,74 @@ $(document).ready(function () {
     // Request Submited to get Route
     $form.submit(function (event) {
         event.preventDefault()
-
-        var departure = $departure.val()
-        var arrival = $arrival.val()
-        var sens = $sens.val()
-
-        var notNow = sens == 'arrive' || sens == 'leave'
-        var time = null
-        var valid = true
-
-        // Inputs verification
-        if (typeof departure !== 'string' || departure.length == 0) {
-            setClassDanger($departure)
-            valid = false
-        }
-
-        if (typeof arrival !== 'string' || arrival.length == 0) {
-            setClassDanger($arrival)
-            valid = false
-        }
-
-        if (notNow) {
-            try {
-                var timeValue = $time.val()
-                var timeValueSplited = timeValue.split(':')
-                var hours = parseInt(timeValueSplited[0], 10)
-                var minutes = parseInt(timeValueSplited[1], 10)
-                if (timeValue.length == 5 && minutes != NaN && hours != NaN) {
-                    time = [hours, minutes]
-                } else
-                    throw 'err'
-            } catch (e) {
-                setClassDanger($time)
-                valid = false
-            }
-        } else {
-            var date = new Date()
-            time = [date.getHours(), date.getMinutes()]
-        }
-
-        if (valid) {
-            // Submiting form
-            requestFactory.route(departure, arrival, sens, time)
+        var validator = formValidator($departure.val(), $arrival.val(), $time.val(), $sens.val())
+        if (validator.valid.all) {
+            var data = validator.data
+            requestFactory.route(data.departure, data.arrival, data.sens, data.time)
                 .done(function (data) {
                     setClassSuccess($departure)
                     setClassSuccess($arrival)
-                    if (notNow)
-                        setClassSuccess($time)
+                    setClassSuccess($time)
                     $result.html(data)
                 })
                 .fail(function (jqXHR) {
-                    var departure = jqXHR.responseJSON.departure,
-                        arrival = jqXHR.responseJSON.arrival
-                    if (!departure) setClassDanger($departure)
-                    if (!arrival) setClassDanger($arrival)
+                    var htmlBld = new HTMLBuilder()
+                        .add('p', 'Thoses stations does not exist')
+                        .open('ul class="uk-margin-remove"')
+
+                    if (!jqXHR.responseJSON.departure) {
+                        setClassDanger($departure)
+                        htmlBld.add('li', 'Departure')
+                    }
+
+                    if (!jqXHR.responseJSON.arrival) {
+                        setClassDanger($arrival)
+                        htmlBld.add('li', 'Arrival')
+                    }
+
+                    htmlBld.close('ul')
+
                     UIkit.notify({
-                        message: "Station(s) do(es) not exist(s)",
+                        message: htmlBld.build(),
                         status: 'danger',
                         timeout: 5000,
                         pos: 'top-center'
-                    });
+                    })
                 })
         } else {
+            var valid = validator.valid
+            var htmlBld = new HTMLBuilder().open('ul class="uk-margin-remove"')
+
+            if (!valid.departure || !valid.arrival) {
+                htmlBld.add('li', 'Please fill in the field(s)').open('ul class="uk-margin-remove"')
+
+                if (!valid.departure) {
+                    setClassDanger($departure)
+                    htmlBld.add('li', 'departure')
+                }
+
+                if (!valid.arrival) {
+                    setClassDanger($arrival)
+                    htmlBld.add('li', 'arrival')
+                }
+                htmlBld.close('ul')
+            } else if (!valid.diffRouteArrival) {
+                setClassDanger($arrival)
+                setClassDanger($departure)
+                htmlBld.add('li', 'The place of departure and arrival must be different.')
+            }
+
+            if (!valid.time) {
+                setClassDanger($time)
+                htmlBld.add('li', 'Time field was not valid')
+            }
+
             UIkit.notify({
-                message: "Please fill in the fields",
+                message: htmlBld.close('ul').build(),
                 status: 'danger',
                 pos: 'top-center',
                 timeout: 5000
-            });
+            })
         }
     })
 })
