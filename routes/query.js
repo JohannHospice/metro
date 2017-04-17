db303c429cf43002a73b7c19c9073f66f093e80d
Johann Hospice <johannhospice.dev@gmail.com>
2017-04-17 17:34:54 +0200

    Clean code...

--- routes/query.js
+++ routes/query.js
@@ -40,38 +40,6 @@ router.get('/route', function (req, res, next) {
     return res.status(500).json(hasLabel)
 });
 
-router.get('/route-pure', function (req, res, next) {
-    let {
-        departure,
-        arrival,
-        time,
-        sens
-    } = req.query
-
-    if (typeof departure !== 'string' ||
-        typeof arrival !== 'string' ||
-        typeof sens !== 'string' ||
-        typeof time !== 'object')
-        return res.status(500).send('not valid')
-
-    departure = departure.trim().toLowerCase()
-    arrival = arrival.trim().toLowerCase()
-
-    let isLeaving = sens == 'now' || sens == 'leave'
-    let hasLabel = {
-        departure: metroManager.hasLabel(departure),
-        arrival: metroManager.hasLabel(arrival)
-    }
-
-    if (hasLabel.arrival && hasLabel.departure) {
-        let nd1 = metroManager.getNodeByLabel(departure)
-        let nd2 = metroManager.getNodeByLabel(arrival)
-        let path = metroManager.buildPath(nd1, nd2, isLeaving, time).pack()
-        res.send(path)
-    } else
-        res.status(500).json(hasLabel)
-});
-
 router.get('/search', function (req, res, next) {
     if (typeof req.query.location !== 'string')
         return res.status(500).send('not valid')
