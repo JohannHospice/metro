027632a7ff4a27617d211b17a6ad72e8c9eb4077
Johann Hospice <johannhospice.dev@gmail.com>
2017-04-17 17:34:54 +0200

    Clean code...

--- app/MetroManager.js
+++ app/MetroManager.js
@@ -4,34 +4,11 @@ const fs = require('fs')
 const MetroGraph = require('./graph/MetroGraph'),
 	PathBuilder = require('./pathfinder/builder/PathBuilder'),
 	MetroPath = require('./pathfinder/object/metro/MetroPath'),
-	tools = require('./tools')
+	tools = require('./tools'),
+	parser = require('./tools/parser')
 
 const REG_FILENAME_STATION = /^metro_ligne(.*?)\.stations$/
 const CACHE_LIFETIME = 10000
-
-const parser = {
-	labels(data) {
-		// [[number, string], ...]
-		return data.split('\n').map(line => {
-			let i = line.indexOf(' ')
-			return [parseInt(line.slice(0, i)), line.slice(i + 1).trim().toLowerCase()]
-		}).filter(label => label.length == 2 && label[0] > -1 && label[1].length > 0)
-	},
-
-	edges(data) {
-		// [[number, number, number], ...]
-		return data.split('\n')
-			.map(line => line.split(' ').map(k => parseInt(k)))
-			.filter(edge => edge.length == 3 && edge[0] > -1 && edge[1] > -1 && edge[2] > -1)
-	},
-
-	station(data) {
-		// [number, ...]
-		return data.split('\n')
-			.map(line => parseInt(line.trim()))
-			.filter(station => station > -1)
-	}
-}
 
 /**
  * Constructor
@@ -88,10 +65,9 @@ MetroManager.prototype.buildPath = function (source, target, isLeaving, time) {
 MetroManager.prototype.suggestions = function (labelReq, max = 25) {
 	let suggestions = []
 	this.metroGraph.forEachLabel((label) => {
-		let distance = tools.distance(labelReq, label)
 		suggestions.push({
 			label,
-			distance
+			distance: tools.distance(labelReq, label)
 		})
 	})
 	return suggestions.filter((thing, index, self) => self.findIndex((t) => t.label === thing.label) === index)
